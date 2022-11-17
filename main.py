8a2a3c0786a421bf0b3700df0343d6ee3e507336
Vasiliy Samoylik <44618834+ToxicVasiliy@users.noreply.github.com>
2022-11-18 00:22:58 +0300

    Done
    Ah shit its done

--- main.py
+++ main.py
@@ -21,14 +21,13 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 dp.middleware.setup(LoggingMiddleware())
 logging.basicConfig(level=logging.INFO)
 admin_keyboard = types.ReplyKeyboardMarkup()
-admin_keyboard.add(types.InlineKeyboardButton(text="Добавить приколюху", callback_data="add_item"))
-admin_keyboard.add(types.InlineKeyboardButton(text="Удалить приколюху", callback_data="delete_item"))
-admin_keyboard.add(types.InlineKeyboardButton(text="Показать приколюхи", callback_data="show_items"))
+admin_keyboard.add(types.InlineKeyboardButton(text="Добавить товар", callback_data="add_item"))
+admin_keyboard.add(types.InlineKeyboardButton(text="Удалить товар", callback_data="delete_item"))
+admin_keyboard.add(types.InlineKeyboardButton(text="Показать товары", callback_data="show_items"))
 admin_keyboard.add(types.InlineKeyboardButton(text="Высрать exel", callback_data="export_to_excel"))
 
 moderator_keyboard = types.ReplyKeyboardMarkup()
-moderator_keyboard.add(types.InlineKeyboardButton(text="Написать остаток приколюх", callback_data="add_mod_amount"))
-moderator_keyboard.add(types.InlineKeyboardButton(text="Показать приколюхи", callback_data="show_items"))
+moderator_keyboard.add(types.InlineKeyboardButton(text="Написать остаток товаров", callback_data="add_mod_amount"))
 
 
 class AdminState(StatesGroup):
@@ -52,7 +51,7 @@ async def start(message: types.Message):
         await message.answer("Дарова модератор", reply_markup=moderator_keyboard)
         await ModeratorState.moderator.set()
     elif message.text == "/start " + admin_password:
-        await message.answer("Бонжур админ", reply_markup=admin_keyboard)
+        await message.answer("Бонсуар, админ", reply_markup=admin_keyboard)
         await AdminState.admin.set()
     else:
         await message.answer("403\nAccess to the requested resource is forbidden")
@@ -60,14 +59,14 @@ async def start(message: types.Message):
 
 @dp.message_handler(state=AdminState.admin)
 async def admin(message: types.Message, state: FSMContext):
-    if message.text == "Добавить приколюху":
-        await message.answer("Введите название приколюхи")
+    if message.text == "Добавить товар":
+        await message.answer("Введите название товара")
         await AdminState.add_item.set()
-    elif message.text == "Удалить приколюху":
-        await message.answer("Введите название приколюхи")
+    elif message.text == "Удалить товар":
+        await message.answer("Введите название товара")
         await AdminState.delete_item.set()
-    elif message.text == "Показать приколюхи":
-        await message.answer("Приколюхи:")
+    elif message.text == "Показать товары":
+        await message.answer("товары:")
         with open("items.json", "r") as file:
             items = json.load(file)
         print(items)
@@ -92,23 +91,57 @@ async def admin(message: types.Message, state: FSMContext):
 
 @dp.message_handler(state=ModeratorState.moderator)
 async def moderator(message: types.Message, state: FSMContext):
-    if message.text == "Написать остаток приколюх":
-        await message.answer("Введите название приколюхи")
-        await ModeratorState.add_mod_amount.set()
-    elif message.text == "Изменить остаток приколюх":
-        await message.answer("Введите название приколюхи")
-        await ModeratorState.edit_mod_amount.set()
-    elif message.text == "Показать приколюхи":
-        await message.answer("Приколюхи:")
+    if message.text == "Написать остаток товаров":
+        items_keyboard = types.ReplyKeyboardMarkup()
+        items_keyboard.add(types.InlineKeyboardButton(text="↩️Назад ↩️", callback_data="back"))
         with open("items.json", "r") as file:
             items = json.load(file)
-        print(items)
         for item in items:
-            await message.answer(item + " " + items[item][0] + " " + items[item][1])
-        await ModeratorState.moderator.set()
+            items_keyboard.add(types.InlineKeyboardButton(text=item, callback_data=item))
+        await message.answer("Выберите название товара", reply_markup=items_keyboard)
+        await ModeratorState.add_mod_amount.set()
+
     else:
         await message.answer("Неверная команда")
         await ModeratorState.moderator.set()
+
+
+# moderator choose item and enters its price
+@dp.message_handler(state=ModeratorState.add_mod_amount)
+async def add_mod_amount(message: types.Message, state: FSMContext):
+    await state.update_data(item=message.text)
+    if message.text != "↩️Назад ↩️":
+        await message.answer("Введите остаток")
+        await ModeratorState.edit_mod_amount.set()
+    else:
+        await message.answer("Назад", reply_markup=moderator_keyboard)
+        await ModeratorState.moderator.set()
+
+
+# moderator enters price
+@dp.message_handler(state=ModeratorState.edit_mod_amount)
+async def edit_mod_amount(message: types.Message, state: FSMContext):
+    try:
+        if message.text != "↩️Назад ↩️":
+            data = await state.get_data()
+            item = data.get("item")
+            with open("items.json", "r") as file:
+                items = json.load(file)
+            items[item][1] = message.text
+            with open("items.json", "w") as file:
+                json.dump(items, file)
+            await message.answer("Остаток изменен")
+            await ModeratorState.add_mod_amount.set()
+        else:
+            await message.answer("Меню", reply_markup=moderator_keyboard)
+            await ModeratorState.moderator.set()
+    except Exception as e:
+        if message.text == "↩️Назад ↩️":
+            await ModeratorState.moderator.set()
+        else:
+            await message.answer("Неверная команда")
+            await ModeratorState.moderator.set()
+
 
 
 @dp.message_handler(state=AdminState.add_item)
@@ -128,7 +161,7 @@ async def add_amount(message: types.Message, state: FSMContext):
     items[item_name] = item_info
     with open("items.json", "w") as file:
         json.dump(items, file)
-    await message.answer("Приколюха добавлена")
+    await message.answer("Товар добавлена")
     await AdminState.admin.set()
 
 
@@ -139,31 +172,8 @@ async def delete_item(message: types.Message, state: FSMContext):
     items.pop(message.text)
     with open("items.json", "w") as file:
         json.dump(items, file)
-    await message.answer("Приколюха удалена")
+    await message.answer("Товар удалена")
     await AdminState.admin.set()
-
-
-@dp.message_handler(state=ModeratorState.add_mod_amount)
-async def add_mod_amount(message: types.Message, state: FSMContext):
-    await message.answer("Введите остаток")
-    await ModeratorState.mod_amount_done.set()
-    await state.update_data(item_name=message.text)
-
-
-@dp.message_handler(state=ModeratorState.mod_amount_done)
-async def mod_amount_done(message: types.Message, state: FSMContext):
-    try:
-        data = await state.get_data()
-        item_name = data.get("item_name")
-        with open("items.json", "r") as file:
-            items = json.load(file)
-            items[item_name][1] = message.text
-            with open("items.json", "w") as file:
-                json.dump(items, file)
-        await message.answer("Остаток изменен")
-    except Exception:
-        await message.answer("Нет такой приколюхи")
-    await ModeratorState.moderator.set()
 
 
 if __name__ == '__main__':
